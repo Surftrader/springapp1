4cbed060b2b90736d763f2865ad9a444a4a3a4ee
Sergiy Poddubka <poseal@gmail.com>
2021-03-09 17:27:53 +0200

    dependency injection via setter

--- src/main/java/ua/com/poseal/springcourse/MusicPlayer.java
+++ src/main/java/ua/com/poseal/springcourse/MusicPlayer.java
@@ -9,7 +9,14 @@ public class MusicPlayer {
         this.music = music;
     }
 
+    public MusicPlayer() {
+    }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
