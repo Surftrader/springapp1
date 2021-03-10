205c24dbd83fbfa974cc6334db98a89fd0870f9b
Sergiy Poddubka <poseal@gmail.com>
2021-03-10 17:46:53 +0200

    @Value

--- src/main/java/ua/com/poseal/springcourse/MusicPlayer.java
+++ src/main/java/ua/com/poseal/springcourse/MusicPlayer.java
@@ -2,10 +2,17 @@ package ua.com.poseal.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+
+    @Value("${musicPlayer.name}")
+    private String name;
+
+    @Value("${musicPlayer.volume}")
+    private int volume;
 
     private Music music1;
     private Music music2;
@@ -20,4 +27,11 @@ public class MusicPlayer {
         return "Playing: " + music1.getSong() + " " + music2.getSong();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
 }
--- src/main/java/ua/com/poseal/springcourse/TestSpring.java
+++ src/main/java/ua/com/poseal/springcourse/TestSpring.java
@@ -7,8 +7,10 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);;
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
