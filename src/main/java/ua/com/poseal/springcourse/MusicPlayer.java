579c14fa418aca95ac79f6ab99efce6d50407458
Sergiy Poddubka <poseal@gmail.com>
2021-03-10 11:49:16 +0200

    Autowired from field

--- src/main/java/ua/com/poseal/springcourse/MusicPlayer.java
+++ src/main/java/ua/com/poseal/springcourse/MusicPlayer.java
@@ -6,21 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
 
+    @Autowired
     private Music music;
-
-    // Ioc
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
 
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
     }
 
-
-    @Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
 }
